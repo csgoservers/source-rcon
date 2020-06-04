f8d823b5148b4f28426df1aeba15042a285868f0
Javi <nhitbh@gmail.com>
2020-06-04 18:21:30 +0200

    Create method to serialize and deserialize packets. Use testify for test.

--- pkg/net/packet.go
+++ pkg/net/packet.go
@@ -1,8 +1,12 @@
 package net
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
+	"math/rand"
 )
 
 const (
@@ -21,10 +25,14 @@ type PacketType int32
 // Packet is the payload that both requests and responses
 // are sent as TCP packets.
 type Packet struct {
-	Size int32
 	ID   int32
 	Type PacketType
 	Body string
+}
+
+// NewPacket creates a default packet
+func NewPacket() *Packet {
+	return &Packet{ID: rand.Int31()}
 }
 
 // Validate validates that the packet size is the expected one.
@@ -34,8 +42,55 @@ func (p *Packet) Validate() error {
 	if len(p.Body) == 0 {
 		return errors.New("packet body is empty")
 	}
-	if int32(len(p.Body))+minimumPacketSize > maximumPacketSize {
+	if p.Size() > maximumPacketSize {
 		return fmt.Errorf("packet size is > %d", maximumPacketSize)
 	}
+	return nil
+}
+
+// String return the packet representation
+func (p *Packet) String() string {
+	return fmt.Sprintf("%08x %08x %08x %v", p.Size(), p.ID, p.Type, []byte(p.Body))
+}
+
+// Serialize transforms the packet to a byte array to be sent to the
+// server.
+func (p *Packet) Serialize() ([]byte, error) {
+	raw := new(bytes.Buffer)
+	binary.Write(raw, binary.LittleEndian, p.Size())
+	binary.Write(raw, binary.LittleEndian, p.ID)
+	binary.Write(raw, binary.LittleEndian, p.Type)
+	binary.Write(raw, binary.LittleEndian, []byte(p.Body))
+	// body payload must be null terminated.
+	binary.Write(raw, binary.LittleEndian, "\x00")
+	binary.Write(raw, binary.LittleEndian, "\x00")
+	return raw.Bytes(), nil
+}
+
+func (p *Packet) bodySize() int32 {
+	return int32(len(p.Body))
+}
+
+// Size computes the total package size
+func (p *Packet) Size() int32 {
+	return (p.bodySize() + minimumPacketSize)
+}
+
+// Deserialize transforms the payload into a readable Packet.
+func (p *Packet) Deserialize(payload []byte) error {
+	data := bytes.NewBuffer(payload)
+
+	var size int32
+	binary.Read(data, binary.LittleEndian, &size)
+	binary.Read(data, binary.LittleEndian, &p.ID)
+	binary.Read(data, binary.LittleEndian, &p.Type)
+	// read body data
+	bodySize := size - minimumPacketSize
+	bodyData := make([]byte, bodySize)
+	_, err := io.ReadFull(data, bodyData)
+	if err != nil {
+		return err
+	}
+	p.Body = string(bodyData)
 	return nil
 }
--- pkg/net/packet_test.go
+++ pkg/net/packet_test.go
@@ -2,26 +2,64 @@ package net
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPacketBodyNotValid(t *testing.T) {
-	packet := Packet{}
+	packet := NewPacket()
 	err := packet.Validate()
-	if err == nil {
-		t.Error(err)
-	}
+	assert.Error(t, err)
 }
 
 func TestPacketSizeGreaterThanAllowed(t *testing.T) {
-	packet := Packet{}
+	packet := NewPacket()
 	b := make([]byte, 4087)
 	for i := range b {
 		b[i] = 'a'
 	}
 	packet.Body = string(b)
-
 	err := packet.Validate()
-	if err == nil {
-		t.Error(err)
-	}
+	assert.Error(t, err)
+}
+
+func TestDefaultPacketSize(t *testing.T) {
+	packet := NewPacket()
+	raw, err := packet.Serialize()
+	assert.Nil(t, err)
+	assert.NotNil(t, raw)
+	assert.Equal(t, 12, len(raw)) // 10 packet + 1 size header + 1 empty string termination
+	assert.Equal(t, int32(10), packet.Size())
+}
+
+func TestSerializePacket(t *testing.T) {
+	packet := NewPacket()
+	packet.Type = serverDataAuth
+	packet.Body = "aaaa"
+
+	raw, err := packet.Serialize()
+	assert.NoError(t, packet.Validate())
+	assert.Nil(t, err)
+	assert.NotNil(t, raw)
+	assert.Equal(t, int32(14), packet.Size())
+	assert.Equal(t, 16, len(raw))
+}
+
+func TestDeserializePacket(t *testing.T) {
+	p1 := NewPacket()
+	p1.Type = serverDataAuth
+	p1.Body = "aaaa"
+
+	raw, err := p1.Serialize()
+	assert.NoError(t, p1.Validate())
+	assert.Nil(t, err)
+	assert.NotNil(t, raw)
+
+	p2 := NewPacket()
+	err = p2.Deserialize(raw)
+	assert.NoError(t, err)
+	assert.Equal(t, "aaaa", p2.Body)
+	assert.Equal(t, int32(14), p2.Size())
+	assert.Equal(t, p1.ID, p2.ID)
+	assert.Equal(t, p1.Type, p2.Type)
 }
